07c60bb8f00efaa52ab154c9598d263ac5981801
Filippo <filippomaschio86@gmail.com>
2024-02-28 15:10:45 -0300

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<App />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <Route path="*" element={<App />}></Route>
+    </Router>
   </React.StrictMode>
 );
